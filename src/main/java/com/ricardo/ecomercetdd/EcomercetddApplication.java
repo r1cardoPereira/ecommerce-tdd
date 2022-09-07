1063de8865cc154d6b2a10012ef71253f014b50a
Ricardo Pereira <103537193+r1cardoPereira@users.noreply.github.com>
2022-09-07 09:20:57 -0300

    iniciando projeto

---
+++ src/main/java/com/ricardo/ecomercetdd/EcomercetddApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.ecomercetdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomercetddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomercetddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/ecomercetdd/EcomercetddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.ecomercetdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomercetddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
