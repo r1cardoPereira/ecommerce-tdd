8aaa8c63c0f20a4bf733b8ddb6e2c4f2d97aa981
Ricardo Pereira <103537193+r1cardoPereira@users.noreply.github.com>
2022-09-13 11:07:00 -0300

    inserindo argumentos

--- criar-imagem-docker.sh
+++ criar-imagem-docker.sh
@@ -1,2 +1,8 @@
-docker build -t r1cardopereira/ecomercetdd
-docker push r1cardopereira/ecomercetdd
+echo "Digite a tag"
+read TAG
+
+git tag $TAG
+git push --tags
+
+docker build -t r1cardopereira/ecomercetdd:$TAG
+docker push r1cardopereira/ecomercetdd:$TAG
